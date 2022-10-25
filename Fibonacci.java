d23ca8f425f95f9350fd573945a2624867748a73
mevshnu <112367169+mevshnu@users.noreply.github.com>
2022-10-25 11:49:45 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Fibonacci{
+    static int fibonacci(int n){
+        if(n == 0){
+            return 0;
+        }
+        if(n == 1 || n == 2){
+            return 1;
+        }
+        return fibonacci(n-2) + fibonacci(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int maxNumber = sc.nextInt();
+        System.out.print("Fibonacci Series of "+maxNumber+" numbers: ");
+        for(int i = 0; i < maxNumber; i++){
+            System.out.print(fibonacci(i) +" ");
+        }
+    }
+}
